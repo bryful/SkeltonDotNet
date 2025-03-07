8e610390654e6fe1167de6ff01a1fb1faf037131
古橋 宏 <bryful@gmail.com>
2025-03-07 18:00:11 +0900

    プロジェクト ファイルを追加します。

---
+++ SkeltonDotNet/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SkeltonDotNet6
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			textBox1 = new TextBox();
+			menuStrip1 = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			quitToolStripMenuItem = new ToolStripMenuItem();
+			menuStrip1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// textBox1
+			// 
+			textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBox1.BackColor = Color.FromArgb(64, 64, 64);
+			textBox1.BorderStyle = BorderStyle.FixedSingle;
+			textBox1.ForeColor = Color.FromArgb(230, 230, 230);
+			textBox1.Location = new Point(12, 62);
+			textBox1.Multiline = true;
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(593, 291);
+			textBox1.TabIndex = 0;
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.Anchor = AnchorStyles.None;
+			menuStrip1.AutoSize = false;
+			menuStrip1.BackColor = SystemColors.Control;
+			menuStrip1.Dock = DockStyle.None;
+			menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 22);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(626, 24);
+			menuStrip1.TabIndex = 1;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.BackColor = SystemColors.Control;
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { quitToolStripMenuItem });
+			fileToolStripMenuItem.ForeColor = SystemColors.ControlText;
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(37, 20);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// quitToolStripMenuItem
+			// 
+			quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+			quitToolStripMenuItem.Size = new Size(97, 22);
+			quitToolStripMenuItem.Text = "Quit";
+			// 
+			// MainForm
+			// 
+			AllowDrop = true;
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(64, 64, 64);
+			ClientSize = new Size(626, 374);
+			Controls.Add(textBox1);
+			Controls.Add(menuStrip1);
+			MainMenuStrip = menuStrip1;
+			Name = "MainForm";
+			Text = "MainForm";
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private TextBox textBox1;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem quitToolStripMenuItem;
+	}
+}
